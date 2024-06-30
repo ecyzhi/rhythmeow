8f7c1bc7425fd14763c8e67a22b56207a762bbe9
ecyzhi <44255766+ecyzhi@users.noreply.github.com>
2024-06-30 12:18:05 +0800

    Built web
    Branch: main
    Commit: 8ad87193ed2bdc01ac7911690ec61a8daf6ef588
    
    package:peanut 5.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3865467714"
+    serviceWorkerVersion: "4189595920"
   }
 });
